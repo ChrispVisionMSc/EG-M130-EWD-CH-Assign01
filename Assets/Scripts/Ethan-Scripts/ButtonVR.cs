47b015b9975d344ab29e639253bcca5dee03422d
UniEthanWD <147525573+UniEthanWD@users.noreply.github.com>
2024-02-22 18:14:51 +0000

    Added unfinished scripts for pressing buttons using VR hands. unfinished becouse the code for moving the machine will be inputed into the buttons.

--- Assets/Scripts/Ethan-Scripts/ButtonVR.cs
+++ Assets/Scripts/Ethan-Scripts/ButtonVR.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ButtonVR : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
+
     void Start()
     {
-        
+        sound = GetComponent<AudioSource>();
+        isPressed = false;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(!isPressed)
+        {
+            Button.transform.localPosition = new Vector3(0f, 0.003f, 0f);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+
+
+        }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == presser) {
+            Button.transform.localPosition = new Vector3(0f, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed=false;
+        
+        }
+    }
+    //public void *insert name here for what the btton does*(){}
+
 }
