380ab638fc9c946de4369d9199b2c54358e14e9e
UniEthanWD <147525573+UniEthanWD@users.noreply.github.com>
2024-02-29 16:35:18 +0000

    fix / tidy up

--- Assets/Scripts/Ethan-Scripts/HeadHitting.cs
+++ Assets/Scripts/Ethan-Scripts/HeadHitting.cs
@@ -1,20 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-public class HeadHitting : MonoBehaviour
-{
-   
+//public class HeadHitting : MonoBehaviour
+//{
+//     public float headHittingCount;
+//    public class SaveObject
+//        {
+
+//            public float headHittingCount;
+//        }
+//    public void save()
+//        {
+//            Debug.Log("saving to " + Application.dataPath);
+
+//            SaveObject saveObject = new SaveObject()
+//            {
+//                headHittingCount = headHittingCount,
+
+//            };
+//            string json = JsonUtility.ToJson(saveObject);
+//            File.WriteAllText(Application.dataPath + "/save.txt", json);
+
+//        }
     public class Entertrigger : MonoBehaviour
     { 
-        public int headHittingCount =0;
+        
         public GameObject redCube;
+        public float headHittingCount;
         // Start is called before the first frame update
-        private void OnTriggerEnter(Collider MainCamera)
+
+
+
+        public void OnTriggerEnter(Collider MainCamera)
         {
             redCube.SetActive(true);
             headHittingCount++;
-            Debug.Log("ouch");
+            Debug.Log("ouch" + headHittingCount + "\n\n");
         }
         public void OnTriggerExit(Collider MainCamera)
         {
